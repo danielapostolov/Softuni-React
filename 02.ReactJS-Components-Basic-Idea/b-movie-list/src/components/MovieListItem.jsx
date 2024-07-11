fda02367c0d08026cce353c54aefc2967467b991
Daniel Apostolov <146780286+danielapostolov@users.noreply.github.com>
2024-07-11 11:55:30 +0300

    Delete 02.ReactJS-Components-Basic-Idea directory

--- 02.ReactJS-Components-Basic-Idea/b-movie-list/src/components/MovieListItem.jsx
+++
@@ -1,9 +0,0 @@
-export default function MovieListItem(props) {
-    return (
-        <li style={{ color: 'green' }}>
-            <a href={props.url || '#'} target="_blank">
-                {props.children}
-            </a>
-        </li>
-    );
-}
